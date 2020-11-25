6eb4bfd00dea2c5ec658146773935066ab881d12
adhess <33546069+adhess@users.noreply.github.com>
2020-11-25 20:30:47 +0100

    make changes to the search bar and moved the inline css to separate file. (#18)
    Co-authored-by: adhess <nicorobine@gmail.com>

--- src/component/app/nav/Nav.tsx
+++ src/component/app/nav/Nav.tsx
@@ -3,67 +3,45 @@ import IconButton from "@material-ui/core/IconButton";
 import SearchIcon from '@material-ui/icons/Search';
 import logo from '../../../assets/images/logo.png'
 import Box from '@material-ui/core/Box';
-import {
-    FormControl,
-    InputAdornment,
-    InputLabel,
-    OutlinedInput,
-} from "@material-ui/core";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import {connect} from "react-redux";
-import styles from "../categories/categories.module.css";
 import {NavLink} from "react-router-dom";
-
+import TextField from "@material-ui/core/TextField";
+import styles from "./nav.module.scss";
 
 class Nav extends Component<any, any> {
     render() {
         return (
-            <div style={{
-                display: 'flex',
-                flexDirection: 'column',
-                width: '100%',
-                height: 'fit-content',
-                top: 0,
-                position: 'fixed',
-                background: '#bde0fe',
-                zIndex: 999,
-                // backgroundImage: 'linear-gradient(180deg, #bde0fe,  #a2d2ff)'
-
-            }}>
-                <Box
-                    // borderBottom="1px solid #eeeeee"
-                    style={{borderBottom: "2px solid rgba(165, 215, 250, .8)"}}
-
-                    width='100%' minHeight={70} display='flex' flexDirection='row' className='shadow'>
+            <div className={styles.container}>
+                <Box className={['shadow',styles.box].join(' ')}>
                     <NavLink to='/'>
-                        <img src={logo} alt="logo" height="35px" style={{margin: '16px 0 auto 16px'}}/>
+                        <img className={styles.logo} src={logo}/>
                     </NavLink>
-                    <div style={{margin: "auto", width: 'fit-content', display: 'flex', flexDirection: 'row'}}>
-                        <FormControl variant="outlined" style={{width: '500px'}}>
-                            <InputLabel htmlFor="search-product">Search for anything</InputLabel>
-                            <OutlinedInput  style={{backgroundColor: 'white'}}
-                                id="search-product"
-                                type='text'
-                                endAdornment={
-                                    <InputAdornment position="end">
-                                        <IconButton edge="end">
-                                            <SearchIcon/>
-                                        </IconButton>
-                                    </InputAdornment>
-                                }
-                                labelWidth={140}
-                                fullWidth={true}
-                            />
-                        </FormControl>
-                        {/*<FormControl variant="outlined" style={{width: '150px', marginLeft: '10px'}}>*/}
-                        {/*    <InputLabel id="id-select-categories">All categories</InputLabel>*/}
-                        {/*    <Select labelId="id-select-categories" id="id-select-categories" labelWidth={100}>*/}
-                        {/*        <MenuItem value="cell_phones">Cell phones & accessories</MenuItem>*/}
-                        {/*        <MenuItem value="computers">Computers</MenuItem>*/}
-                        {/*    </Select>*/}
-                        {/*</FormControl>*/}
-                        {this.props.async_counter}
-                    </div>
+                    <TextField
+                        label="Search for anything"
+                        margin="dense"
+                        variant="outlined"
+                        className={styles.searchText}
+                        InputProps={{
+                            style: {
+                                height: '40px',
+                                padding: '0 14px',
+                            },
+                            endAdornment: (
+                                    <IconButton>
+                                        <SearchIcon />
+                                    </IconButton>
+                            )
+                        }}
+                    />
+
+                    {/*<FormControl variant="outlined" style={{width: '150px', marginLeft: '10px'}}>*/}
+                    {/*    <InputLabel id="id-select-categories">All categories</InputLabel>*/}
+                    {/*    <Select labelId="id-select-categories" id="id-select-categories" labelWidth={100}>*/}
+                    {/*        <MenuItem value="cell_phones">Cell phones & accessories</MenuItem>*/}
+                    {/*        <MenuItem value="computers">Computers</MenuItem>*/}
+                    {/*    </Select>*/}
+                    {/*</FormControl>*/}
                     <div style={{width: '131px'}}/>
                 </Box>
                 {
