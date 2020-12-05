dcc32a246b4d737e2e93c82c78d33022b0a0af0a
adhess <nicorobine@gmail.com>
2020-12-05 23:58:11 +0100

    -) modify category size. -) modify category and navbar background color. -) add show category button.

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,6 @@
 const initialState = {
-    async_counter: 0
+    async_counter: 0,
+    is_show_category: false
 }
 const reducer = (state = initialState, action) => {
     switch (action.type) {
@@ -12,6 +13,11 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 async_counter: state.async_counter - 1
+            }
+        case 'TOGGLE_SHOW_CATEGORY':
+            return {
+                ...state,
+                is_show_category: !state.is_show_category
             }
         default:
             return state;
