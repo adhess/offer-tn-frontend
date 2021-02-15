4ff443cd4ba3a4f674d320a4425b1e72a57aca33
adhess <nicorobine@gmail.com>
2021-02-15 21:54:13 +0100

    landing page

--- src/component/app/home/header/header.tsx
+++ src/component/app/home/header/header.tsx
@@ -13,12 +13,8 @@ class Header extends Component<any, any> {
             <img className={styles.headerImg} src={header} alt=""/>
             <div className={styles.center_section}>
                 <div className={styles.description}>
-                    <h1>Welcome to your trustful products comparator.</h1>
-                    <div className={styles.title}>
-                        <h3>With</h3>
-                        <h3>offer<span>.tn</span></h3>
-                        <h3>you don't need to spend more than what you should.</h3>
-                    </div>
+                    <h2>Welcome to your trustful products comparator</h2>
+                    <h4>Don't spend more than what you should</h4>
                 </div>
                 <img className={styles.heroImg} src={hero} alt=""/>
             </div>
